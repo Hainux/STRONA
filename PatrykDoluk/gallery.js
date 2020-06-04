96878dcad8464e97b6a527a7807071b8ab00e277
PatrykDoluk <66427177+Hainux@users.noreply.github.com>
2020-06-04 16:24:47 +0200

    Initial commit

---
+++ PatrykDoluk/gallery.js
@@ -0,0 +1,7 @@
+function myFunction(imgs)
+{
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    espandImg.parentElement.style.display = "block";
+    
+}
